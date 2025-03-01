3a937072c4f95b45fea7a7db42117bbb38e27961
bconcepcion <bconcepcion1692@gmail.com>
2025-02-28 21:09:01 -0500

    logic for char function

--- stats.py
+++ stats.py
@@ -4,3 +4,22 @@ def get_num_words(book_text):
     text = book_text.split()
 
     return len(text)
+
+
+
+def char_count(str):
+
+    # count var
+
+    str = str.lower()
+
+    for i in range(len(str)):
+        if i == 'a':
+        
+        #count all instances of the char
+        #add that count to the dictionary at the index of char
+        #return dictionary
+
+
+
+    return count
